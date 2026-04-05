f3fdcc34a5f5a61a9cf5336d74132a95a2fba8ed
Arseny <arsenyfrl@gmail.com>
2026-04-05 10:50:21 +0300

    splitting into folders

---
+++ API.py
@@ -0,0 +1,9 @@
+TOKEN = "8765454976:AAGolrqZ8bcQJTj8FnCN1ltagpM0TBbNGDk"
+
+YANDEX_API_KEY = "AQVN3pZhoIJM9PX_TV-U11qoIbKYsuNlhCCk0048"  
+YANDEX_FOLDER_ID = "b1gn6ped0id50712c11g"
+
+MONTHS_RU = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+             'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
+
+CHOOSING_GIFT, WAITING_PREFERENCES, GETTING_NEW_PREFERENCES = range(3)
